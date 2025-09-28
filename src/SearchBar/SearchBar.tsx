65b699357485b4ccf9b263f318d8685e6f784e27
Yevhen Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-09-28 05:03:16 +0300

    init branch

--- src/SearchBar/SearchBar.tsx
+++ src/SearchBar/SearchBar.tsx
@@ -9,28 +9,60 @@ import { makeFilterOptions } from '../utils/makeFilterOptions';
 import type { StateUserEntity } from '../redux/api/types';
 import type { FilterOptionsState } from '@mui/material/useAutocomplete';
 import { setFilteredUsers } from '../redux/users/slice';
+import { LuSearch } from 'react-icons/lu';
+import InputAdornment from '@mui/material/InputAdornment';
 
 const SearchBar: React.FC = () => {
   const userOptions = useSelector(selectUsers);
   const dispatch = useDispatch<AppDispatch>();
   return (
-    <div className={css.SearchBarContainer}>
-      <Autocomplete
-        freeSolo
-        options={userOptions}
-        onInputChange={(_, newInputValue) => {
-          dispatch(setSearchQuery(newInputValue));
-        }}
-        getOptionLabel={option => (typeof option === 'string' ? option : option.name)}
-        filterOptions={(options: StateUserEntity[], state: FilterOptionsState<StateUserEntity>) => {
-          const filter = makeFilterOptions<StateUserEntity>(['name', 'username', 'email', 'phone']);
-          const filteredOptions = filter(options, state);
-          dispatch(setFilteredUsers(filteredOptions));
-          return filteredOptions;
-        }}
-        renderInput={params => <TextField {...params} label="Search users..." variant="outlined" />}
-      />
-    </div>
+    userOptions.length && (
+      <div className={css.SearchBarContainer}>
+        <Autocomplete
+          freeSolo
+          options={userOptions}
+          onInputChange={(_, newInputValue) => {
+            dispatch(setSearchQuery(newInputValue));
+          }}
+          getOptionLabel={option => (typeof option === 'string' ? option : option.name)}
+          // filterOptions={makeFilterOptions<StateUserEntity>(['name', 'username', 'email', 'phone'])}
+          filterOptions={(
+            options: StateUserEntity[],
+            state: FilterOptionsState<StateUserEntity>
+          ) => {
+            const filter = makeFilterOptions<StateUserEntity>([
+              'name',
+              'username',
+              'email',
+              'phone',
+            ]);
+            const filteredOptions = filter(options, state);
+            dispatch(setFilteredUsers(filteredOptions));
+            return filteredOptions;
+          }}
+          renderInput={params => (
+            <TextField
+              {...params}
+              label="Search users..."
+              variant="outlined"
+              className={css.searchContainer}
+              slotProps={{
+                input: {
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <div className={css.searchIcon}>
+                        <LuSearch />
+                      </div>
+                    </InputAdornment>
+                  ),
+                  className: css.searchAutocompleteInput,
+                },
+              }}
+            />
+          )}
+        />
+      </div>
+    )
   );
 };
 
